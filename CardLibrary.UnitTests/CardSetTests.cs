5f57d74dd5590834d61921ede51aebdfe49e34bd
asathkumara <sathkumaraasel@gmail.com>
2019-03-25 01:50:23 -0700

    Added a few more unit tests for the CardSet class GetOneCard() and ResetUsage() methods.

--- CardLibrary.UnitTests/CardSetTests.cs
+++ CardLibrary.UnitTests/CardSetTests.cs
@@ -52,5 +52,45 @@ namespace CardLibrary.UnitTests
 
         }
 
+        [Test]
+        public void ResetUsage_ValidateInPlayReset_ReturnsTrue()
+        {
+            CardSet testdeck = new CardSet();
+
+            testdeck.ResetUsage();
+
+            bool result = false;
+
+            for (int i = 0; i < testdeck.cardArray.Length; i++)
+            {
+                if (testdeck.cardArray[i].Inplay == false)
+                {
+                    result = true;
+                }
+            }
+
+            Assert.That(result = true);
+        }
+
+        [Test]
+        public void GetOneCard_ValidateInPlay_ReturnsTrue()
+        {
+            CardSet testdeck = new CardSet();
+
+            SuperCard[] testHand = testdeck.GetCards(5);
+
+            bool result = false;
+
+            for (int i = 0; i < testHand.Length; i++)
+            {
+                if (testHand[i].Inplay == true)
+                {
+                    result = true;
+                }
+            }
+
+            Assert.That(result = true);
+        }
+
     }
 }
